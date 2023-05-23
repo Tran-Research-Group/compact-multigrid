ad00c585fbdefed7037b1d44dd172ff9651b7b79
Mikihisa Yuasa <myuasa2@illinois.edu>
2023-05-23 12:20:37 -0500

    fix: correct dict attr

--- compact_multigrid/typing/metadata.py
+++ compact_multigrid/typing/metadata.py
@@ -1,9 +1,10 @@
-from typing import  Any, Literal, TypeAlias
+from typing import Any, Literal, TypeAlias
 from typing_extensions import TypedDict, NotRequired
 
-RenderMode: TypeAlias = Literal["human","rgb_array","ansi","ascii"]
+RenderMode: TypeAlias = Literal["human", "rgb_array", "ansi", "ascii"]
+
 
 class Metadata(TypedDict):
-    "render_modes": list[RenderMode]
-    "render_fps": NotRequired[int]
-    "options": NotRequired[dict[str, Any]]
+    render_modes: list[RenderMode]
+    render_fps: NotRequired[int]
+    options: NotRequired[dict[str, Any]]
